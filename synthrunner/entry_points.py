905222ccc9c1e41dd28174e7d8981b7b2c4c6ce9
Gaurav Saple <gsaple@cisco.com>
2021-05-27 09:14:14 -0700

    Added kafka send on event hook for request

--- synthrunner/entry_points.py
+++ synthrunner/entry_points.py
@@ -5,71 +5,60 @@ synthrunner.entry_points.py
 This module contains the entry-point functions for the synthrunner module,
 that are referenced in setup.py.
 """
-from functools import wraps
-import logging
-import configargparse
+import json
+import locust_plugins
+import os
+import time
+from locust.main import main as locust_main
+from dotenv import load_dotenv
 from locust import events
-from locust.exception import StopUser
-from locust.env import Environment
-from locust.runners import Runner
-from locust import TaskSet
-from locust.user.task import DefaultTaskSet
-import locust.main
+from kafka import KafkaProducer
 
-@events.init_command_line_parser.add_listener
-def add_checks_arguments(parser: configargparse.ArgumentParser):
-    other = parser.add_argument_group(
-        "locust-plugins - Extras",
-    )
-    # fix for https://github.com/locustio/locust/issues/1085
-    other.add_argument(
-        "-i",
-        "--iterations",
-        type=int,
-        help="Dont run more than this number of task iterations and terminate once they have finished",
-        env_var="LOCUST_ITERATIONS",
-        default=0,
-    )
-    other.add_argument(
-        "--console-stats-interval",
-        type=int,
-        help="Interval at which to print locust stats to command line",
-        env_var="LOCUST_CONSOLE_STATS_INTERVAL",
-        default=locust.stats.CONSOLE_STATS_INTERVAL_SEC,
-    )
+def push_telemetry(name, val, label_dict):
+    """ Push data to kafka topic """
+    kafka_nodes = os.environ.get('KAFKA_BOOTSTRAP_NODES', '').split(',')
+    kafka_topic = os.environ.get('KAFKA_TOPIC')
+    if not(kafka_nodes and kafka_topic):
+        # telemetry kafka config not initialized
+        return
+    message = {
+        'name': name,
+        'type': 'histogram',
+        'value': 0 if val else 1,
+        'documentation': 'Synthetic Run Result',
+        'timestamp': round(time.time() * 1000),
+        'labels': label_dict or {}
+    }
+    producer = KafkaProducer(bootstrap_servers=kafka_nodes,
+                             value_serializer=lambda v:
+                             json.dumps(v).encode('utf-8'))
+    producer.send(kafka_topic, message)
+    producer.flush()
 
+@events.request.add_listener
+def my_request_handler(request_type, name, response_time, response_length, response,
+                       context, exception, **kwargs):
+    if exception:
+        print(f"Request to {name} failed with exception {exception}")
+    else:
+        print(f"Successfully made a request to: {name}")
 
-@events.test_start.add_listener
-def set_up_iteration_limit(environment: Environment, **_kwargs):
-    options = environment.parsed_options
-    locust.stats.CONSOLE_STATS_INTERVAL_SEC = environment.parsed_options.console_stats_interval
-    if options.iterations:
-        runner: Runner = environment.runner
-        runner.iterations_started = 0
-        runner.iteration_target_reached = False
-
-        def iteration_limit_wrapper(method):
-            @wraps(method)
-            def wrapped(self, task):
-                if runner.iterations_started == options.iterations:
-                    if not runner.iteration_target_reached:
-                        runner.iteration_target_reached = True
-                        logging.info(
-                            f"Iteration limit reached ({options.iterations}), stopping Users at the start of their next task run"
-                        )
-                    if runner.user_count == 1:
-                        logging.info("Last user stopped, quitting runner")
-                        runner.quit()
-                    raise StopUser()
-                runner.iterations_started = runner.iterations_started + 1
-                method(self, task)
-
-            return wrapped
-
-        # monkey patch TaskSets to add support for iterations limit. Not ugly at all :)
-        TaskSet.execute_task = iteration_limit_wrapper(TaskSet.execute_task)
-        DefaultTaskSet.execute_task = iteration_limit_wrapper(DefaultTaskSet.execute_task)
-
+    rval = False if exception else response.ok
+    # Send telemetry data
+    labels = {
+        'endpoint': name,
+        'method': request_type,
+        'version': os.environ.get('VERSION', 'unknown'),
+        'status': 'SUCCESS' if not exception and response.ok else 'FAILURE',
+        'status_code': response.status_code,
+        'env': os.environ.get('ENV', 'STAGE'),
+        'hostname': os.environ.get('HOST'),
+        'site': os.environ.get('SITE', 'unknown'),
+        'realUser': None,
+        'processUser': os.environ.get('USER', 'ngdevx'),
+        'runMode': "REST"
+    }
+    push_telemetry('synthetic_run', rval, labels)
 
 
 def main() -> None:
@@ -79,7 +68,11 @@ def main() -> None:
     """
 
     try:
-        locust.main.main()
+        load_dotenv()
+        # Ensure that number of locust iterations is set, defaults to 1
+        # Synthetic runner must not spawn new instances unless iterations > 1
+        os.environ.setdefault('LOCUST_ITERATIONS', '1')
+        locust_main()
     except IndexError:
         RuntimeError('please supply a command for synthrunner - e.g. install.')
     return None

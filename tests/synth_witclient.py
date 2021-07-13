fe7e916a6df957959bdfc9fde26f32358f585ba7
Gaurav Saple <gsaple@cisco.com>
2021-07-13 16:04:04 -0700

    Add debug messages, use subprocess.run to simply, read wit token file

--- tests/synth_witclient.py
+++ tests/synth_witclient.py
@@ -1,8 +1,11 @@
 import json
+import logging
 import time
-import subprocess as sp
+import subprocess
 from locust import task, between, tag
 from contrib.users import CLIUser
+
+log = logging.getLogger(__name__)  # pylint: disable=locally-disabled, invalid-name
 
 
 class WITCLIUser(CLIUser):
@@ -19,40 +22,37 @@ class WITCLIUser(CLIUser):
     def del_spaces(self):
         print("WITCLIUser: del_spaces")
         for spc in self.spaces:
-            del_process = sp.Popen('wit space delete -noprompt -space %s' % spc,
-                                    shell=True, stdout=sp.PIPE,
-                                    stderr=sp.PIPE)
-            output, error = del_process.communicate()
+            wit_spc_del_cmd = 'wit space delete -noprompt -space %s' % spc
+            del_process = subprocess.run(wit_spc_del_cmd.split(), capture_output=True)
             if del_process.returncode == 0:
-                print("Successfully deleted WIT space - " + spc)
+                log.debug("Successfully deleted WIT space - " + spc)
 
     @task
     def wit_space_list(self, space=None, send_result=True):
         """ Test the wit space list command """
-        print("WITCLIUser: space list")
+        log.debug("WITCLIUser: space list")
         command = "wit space list"
         if space:
             args = "-space %s --json" % space
         else:
             args = "--json"
         with self.client.execute(command, args, catch_response=True) as cli:
-            stdout = cli.output.decode('utf-8')
-            stderr = cli.error.decode('utf-8')
+            stdout = cli.output
+            stderr = cli.error
             if cli.failed:
                 cli.failure(cli.failed, stderr)
                 return cli.failed
             results = json.loads(stdout)['cli_results']
             for x in results:
                 vol_path = x['Storage Path']
-                process = sp.Popen('touch %s/test_wr' % vol_path, shell=True, stdout=sp.PIPE,
-                                   stderr=sp.PIPE)
-                output, error = process.communicate()
+                touch_cmd = 'touch %s/test_wr' % vol_path
+                process = subprocess.run(touch_cmd.split(), capture_output=True)
                 if process.returncode == 0:
+                    log.debug("Space is writable")
+                    clean_cmd = 'rm %s/test_wr' % vol_path
+                    subprocess.run(clean_cmd.split())
                     if send_result:
                         cli.success()
-                    process = sp.Popen('rm %s/test_wr' % vol_path, shell=True, stdout=sp.PIPE,
-                                       stderr=sp.PIPE)
-                    output, error = process.communicate()
                 else:
                     if send_result:
                         cli.failure(process.returncode, "WIT space not writable")
@@ -62,7 +62,7 @@ class WITCLIUser(CLIUser):
     @task
     def wit_space_create(self):
         """ Test the wit space create command """
-        print("WITCLIUser: wit space create")
+        log.debug("WITCLIUser: wit space create")
         command = "wit space create"
         space_name = "mytest_%d" % int(time.time())
         args = "-space %s -team wit --json" % space_name
@@ -77,5 +77,5 @@ class WITCLIUser(CLIUser):
                     cli.command = command
                     cli.failure(list_failed, "WIT space not writable")
             else:
-                cli.failure(cli.failed, cli.error.decode('utf-8'))
+                cli.failure(cli.failed, cli.error)
             self.del_spaces()
--- tests/synth_withttpclient.py
+++ tests/synth_withttpclient.py
@@ -1,7 +1,11 @@
 import os
+import logging
 import time
-import subprocess as sp
-from locust import HttpUser, task, between, tag
+import subprocess
+from urllib.parse import urlparse
+from locust import HttpUser, task, tag
+
+log = logging.getLogger(__name__)  # pylint: disable=locally-disabled, invalid-name
 
 
 class WITHTTPUser(HttpUser):
@@ -9,8 +13,22 @@ class WITHTTPUser(HttpUser):
     def on_start(self):
         self.volumes = []
         self.username = os.environ.get('WIT_USER', 'nobody')
-        self.api_key = os.environ.get('WIT_APIKEY', '')
+        self.api_key = os.environ.get('WIT_APIKEY', self._read_token(self.host))
         self.client.headers = {"Authorization": "Token %s:%s" % (self.username, self.api_key)}
+
+    @staticmethod
+    def _read_token(host):
+        wit_dotpath = os.path.expanduser('~/.wit')
+        hostname = urlparse(host).hostname
+        token_file = "authorization.token." + hostname
+        try:
+            with open(os.path.join(wit_dotpath, token_file), "r") as tokenf:
+                token = tokenf.read()
+                token = token.strip()
+                return token
+        except OSError as err:
+            log.debug("Cannot find token: %s " % str(err))
+            return ""
 
     def get_team(self, name):
         res = self.client.get("/api/wit/team/?group__name=%s" % name)
@@ -23,7 +41,6 @@ class WITHTTPUser(HttpUser):
     def del_volumes(self):
         for vol in self.volumes:
             self.client.delete('/api/wit/volume/%d' % vol)
-
 
     @tag('synthtest')
     @task
@@ -40,22 +57,25 @@ class WITHTTPUser(HttpUser):
         }
         with self.client.post('/api/wit/volume/', json=data, catch_response=True) as response:
             if response.ok:
-                print("Volume created: %s" % spc_name)
+                log.debug("Volume created: %s" % spc_name)
                 vol_data = response.json()
                 self.volumes.append(vol_data['id'])
                 vol_path = "%s/%s" % (vol_data['node']['cluster']['cluster_mount'],
                                       vol_data['junction_path'])
-                process = sp.Popen('touch %s/test_wr' % vol_path, shell=True, stdout=sp.PIPE,
-                                   stderr=sp.PIPE)
-                output, error = process.communicate()
+                touch_cmd = 'touch %s/test_wr' % vol_path
+                process = subprocess.run(touch_cmd.split(), capture_output=True)
                 if process.returncode == 0:
                     response.success()
-                    process = sp.Popen('rm %s/test_wr' % vol_path, shell=True, stdout=sp.PIPE,
-                                       stderr=sp.PIPE)
-                    output, error = process.communicate()
+                    log.debug("Space is writable")
+                    clean_cmd = 'rm %s/test_wr' % vol_path
+                    subprocess.run(clean_cmd.split())
                     self.del_volumes()
                 else:
                     self.del_volumes()
+                    err_msg = "Space is not writable."
+                    log.debug(err_msg)
+                    log.debug("STDOUT: " + process.stdout.decode('utf-8'))
+                    log.debug("STDERR: " + process.stderr.decode('utf-8'))
                     response.failure("Space is not writable")
             else:
                 self.del_volumes()
